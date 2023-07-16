f37ff87d9e3d3d74dfd5a403e1c635e2e4ad7a1e
MH <gmhmhmhg.dev@gmail.com>
2023-07-17 01:36:07 +0900

    setting

---
+++ src/test/java/com/mhmhmh/minibank/MiniBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhmhmh.minibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
